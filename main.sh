47ea08f224d759465652d6408efcf7c5513dcd85
art32fil <art32fiL@gmail.com>
2017-08-23 15:03:44 +0300

    yes answer by default in apt-get

--- main.sh
+++ main.sh
@@ -3,10 +3,10 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-jade-ros-base
+sudo apt-get -y install ros-jade-ros-base
 sudo rosdep init
 rosdep update
 sudo echo "source /opt/ros/jade/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 source /opt/ros/jade/setup.bash
-sudo apt-get install python-rosinstall
+sudo apt-get -y install python-rosinstall
